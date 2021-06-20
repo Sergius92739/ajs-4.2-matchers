353196856840135ec0127c41ae68def9a3f4cbe4
Sergius92739 <sergem279@gmail.com>
2021-06-20 18:36:38 +0300

    1 commit

---
+++ src/__test__/playerList.test.js
@@ -0,0 +1,17 @@
+import sortPlayerList from '../js/playerList';
+
+test('Проверяем корректность сортировки', () => {
+  const data = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  expect(sortPlayerList(data)).toEqual(sortPlayerList(expected));
+});
